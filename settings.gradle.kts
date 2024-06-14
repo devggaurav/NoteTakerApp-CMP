566225b4bf81ac3cd8480ac313ebe223d23c5f40
gc <dev.gauravchauhan@gmail.com>
2024-06-14 19:55:15 +0530

    adding dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,10 @@ pluginManagement {
             }
         }
         mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/wasm/experimental")
+        maven( "https://androidx.dev/storage/compose-compiler/repository")
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
         gradlePluginPortal()
     }
 }
@@ -25,6 +29,10 @@ dependencyResolutionManagement {
             }
         }
         mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/wasm/experimental")
+        maven( "https://androidx.dev/storage/compose-compiler/repository")
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
     }
 }
 
